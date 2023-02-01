adadb2c88343b87773257be6b0de38a4366cb973
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2023-02-01 10:56:35 +0100

    fonts

--- lib/config/themes/app_theme.dart
+++ lib/config/themes/app_theme.dart
@@ -3,9 +3,17 @@ import 'package:quotes_clean/core/utils/app_colors.dart';
 
 ThemeData appTheme() {
   return ThemeData(
-      useMaterial3: true,
-      primaryColor: AppColors.primary,
-      hintColor: AppColors.hint,
-      brightness: Brightness.light,
-      scaffoldBackgroundColor: Colors.white);
+    useMaterial3: true,
+    primaryColor: AppColors.primary,
+    hintColor: AppColors.hint,
+    brightness: Brightness.light,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: const TextTheme(
+      labelLarge: TextStyle(
+        fontSize: 16,
+        color: Colors.black,
+        fontWeight: FontWeight.w500,
+      ),
+    ),
+  );
 }
