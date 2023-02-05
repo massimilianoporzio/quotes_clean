48edb62501501a5fa1059af71e059b3aabdc63a0
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2023-02-05 14:57:56 +0100

    cached

--- lib/core/utils/app_strings.dart
+++ lib/core/utils/app_strings.dart
@@ -1,4 +1,6 @@
 class AppStrings {
   static const String appName = 'Quotes';
+  static const String fontFamily = 'Tajwal';
   static const String noRouteFound = 'No route found';
+  static const String cachedRandomQuote = 'CACHED_RANDOM_QUUOTE';
 }
