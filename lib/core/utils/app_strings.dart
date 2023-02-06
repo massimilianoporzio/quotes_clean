e2d8f8cc0cf23a21c33772bae50838e062341680
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2023-02-06 10:56:11 +0100

    cubit del caso getRandomQuote

--- lib/core/utils/app_strings.dart
+++ lib/core/utils/app_strings.dart
@@ -4,4 +4,9 @@ class AppStrings {
   static const String noRouteFound = 'No route found';
   static const String cachedRandomQuote = 'CACHED_RANDOM_QUUOTE';
   static const String contentType = 'application/json';
+  static const String serverFailure = 'Server Failure';
+  static const String cacheFailure = 'Cache Failure';
+  static const String parsingFailure =
+      'Error in parsing the response from the Server';
+  static const String genericError = 'Oops - something wrong. Retry.';
 }
--- lib/features/random_quote/presentation/cubit/random_quote_cubit.dart
+++ lib/features/random_quote/presentation/cubit/random_quote_cubit.dart
@@ -1,8 +1,43 @@
+import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quotes_clean/core/error/failures.dart';
+import 'package:quotes_clean/core/usecases/base_usecase.dart';
+import 'package:quotes_clean/core/utils/app_strings.dart';
+import 'package:quotes_clean/features/random_quote/domain/usecases/get_random_quote.dart';
+
+import '../../domain/entities/quote.dart';
 
 part 'random_quote_state.dart';
 
 class RandomQuoteCubit extends Cubit<RandomQuoteState> {
-  RandomQuoteCubit() : super(RandomQuoteInitial());
+  final GetRandomQuoteUseCase usecase;
+
+  RandomQuoteCubit({
+    required this.usecase,
+  }) : super(RandomQuoteInitial());
+
+  //*metodi del cubit:
+  Future<void> getRandomQuote() async {
+    //*chiamo il caso d'uso
+    Either<Failure, Quote> response = await usecase(NoParams());
+    //*guardo i casi:
+    emit(response.fold(
+      (failure) => RandomQuoteError(msg: _mapFailureToMsg(failure)),
+      (quote) => RandomQuoteLoaded(quote: quote),
+    ));
+  }
+
+  String _mapFailureToMsg(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return AppStrings.serverFailure;
+      case ParsingFailure:
+        return AppStrings.parsingFailure;
+      case CacheFailure:
+        return AppStrings.cacheFailure;
+      default:
+        return AppStrings.genericError;
+    }
+  }
 }
