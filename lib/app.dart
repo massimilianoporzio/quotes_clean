b186bd26b318d57c84ed7cdcf6f7cd1a1412b2f3
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2023-02-06 11:42:11 +0100

    import non usati

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quotes_clean/config/routes/app_routes.dart';
 import 'package:quotes_clean/config/themes/app_theme.dart';
-import 'package:quotes_clean/core/utils/app_colors.dart';
 import 'package:quotes_clean/core/utils/app_strings.dart';
-import 'package:quotes_clean/core/utils/hex_color.dart';
-import 'package:quotes_clean/features/random_quote/presentation/pages/quote_page.dart';
 
 class QuoteApp extends StatelessWidget {
   const QuoteApp({super.key});
